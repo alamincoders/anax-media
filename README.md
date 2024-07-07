e897449b2e969853e4cf2fe4351b853d6c416de5
asfak00 <asfakahmed680@gmail.com>
2024-07-07 22:32:15 +0600

    project initilize

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to ZenUI free templates organization
+
+Have a nice day with ZenUI Library. visit our website https://zenui.net to get more components & templates.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='text-5xl font-bold flex items-center justify-center h-[100vh]'>
+            Hello Anax Media
+        </div>
+    );
+};
+
+export default App;
