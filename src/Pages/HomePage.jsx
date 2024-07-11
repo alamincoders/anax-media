f4d0c68ad94e4f91e6fdd0b6aa0832e517ae115d
asfak00 <asfakahmed680@gmail.com>
2024-07-12 01:42:19 +0600

    add: about us section service section added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,12 +4,20 @@ import React from 'react';
 import Navbar from '../Shared/Navbar.jsx'
 import Footer from '../Shared/Footer.jsx'
 import Hero from '../Components/Home/Hero.jsx'
+import Company from '../Components/Home/Company.jsx'
+import Overview from '../Components/Home/Overview.jsx'
+import Aboutus from '../Components/Home/Aboutus.jsx'
+import OurService from '../Components/Home/OurService.jsx'
 
 const HomePage = () => {
     return (
         <>
             <Navbar/>
             <Hero/>
+            <Company/>
+            <Overview/>
+            <Aboutus/>
+            <OurService/>
             <Footer/>
         </>
     );
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // react icons
 import { CiSearch } from "react-icons/ci";
-import {styles} from "../Utils/Styles.js";
-import {Link} from "react-router-dom";
+
+// utils
+import { styles } from "../Utils/Styles.js";
+
+// react router dom
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const [shadow, setShadow] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 20) {
+                setShadow(true);
+            } else {
+                setShadow(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className={`flex items-center justify-between px-12 py-4`}>
-            <img src="/logo.png" alt="logo" className='w-[120px]'/>
+        <nav className={`flex items-center justify-between px-12 py-4 sticky top-0 bg-white transition-shadow ${shadow ? 'shadow-lg' : ''}`}>
+            <img src="/logo.png" alt="logo" className='w-[120px]' />
             <ul className='flex items-center gap-[50px]'>
                 <li className='navItem'>
                     <Link to='/'>
@@ -33,7 +55,7 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-[20px]'>
-                <CiSearch className='text-[1.5rem] cursor-pointer'/>
+                <CiSearch className='text-[1.5rem] cursor-pointer' />
                 <button className={`${styles.buttonPrimary}`}>Get Started</button>
             </div>
         </nav>
